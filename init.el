726b246c83542e4ecdaa18544721ec882d470d80
Sean Wenzel <sean.d.wenzel@gmail.com>
2021-03-10 23:39:51 -0800

    Add early-init

--- init.el
+++ init.el
@@ -1,5 +1,21 @@
-(setq gc-cons-threshold 50000000) ;; 50 MB
-(setq large-file-warning-threshold 100000000)
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, and then reset it later
+;; using a hook.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; Keep a ref to the actual file-name-handler
+(defvar default-file-name-handler-alist file-name-handler-alist)
+
+;; Set the file-name-handler to nil (because regexing is cpu intensive)
+(setq file-name-handler-alist nil)
+
+;; Reset file-name-handler-alist after initialization
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216
+          gc-cons-percentage 0.1
+          file-name-handler-alist default-file-name-handler-alist)))
 
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
