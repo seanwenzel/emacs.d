d274b9ca7de30ba16eebff13dbfe80993f2bb91a
Sean Wenzel <sean.d.wenzel@jpl.nasa.gov>
2021-03-18 14:02:06 -0700

    add projectile directories

--- init.el
+++ init.el
@@ -262,6 +262,7 @@
 (use-package projectile
   :ensure t
   :config
+  (setq projectile-project-search-path '("~/projects/" "~/repositories/"))
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
