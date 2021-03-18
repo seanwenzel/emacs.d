2b95ed7cfcd2ef2e7446eed5df15d9d86a6a26e0
Sean Wenzel <sean.d.wenzel@jpl.nasa.gov>
2021-03-18 14:09:22 -0700

    format-all-mode

--- init.el
+++ init.el
@@ -244,7 +244,6 @@
   )
 
 (use-package restart-emacs
-
   :ensure t
   )
 
@@ -339,10 +338,14 @@
   (setq writeroom-width 100)
   )
 
-(use-package prettier-js
-  :ensure t
-  :config
-  (add-hook 'js-mode-hook 'prettier-js-mode))
+(use-package format-all
+  :ensure t)
+
+;; Use format-all-mode instead
+;; (use-package prettier-js
+;;   :ensure t
+;;   :config
+;;   (add-hook 'js-mode-hook 'prettier-js-mode))
 
 (use-package lua-mode
   :ensure t)
@@ -594,7 +597,7 @@ Create prefix map: +general-global-NAME. Prefix bindings in BODY with INFIX-KEY.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(prettier-js lua-mode cmake-mode dockerfile-mode dockerfile py-yapf python-black evil-snipe vlf docker projectile orderless forge json-mode markdown-mode js2-refactor rainbow-delimiters objed smooth-scrolling yasnippet-snippets writeroom-mode which-key use-package robot-mode restart-emacs magit leetcode ivy-yasnippet general flycheck exec-path-from-shell evil-surround evil-commentary evil-collection evil-args editorconfig doom-themes doom-modeline diminish diff-hl counsel company avy add-node-modules-path)))
+   '(format-all prettier-js lua-mode cmake-mode dockerfile-mode dockerfile py-yapf python-black evil-snipe vlf docker projectile orderless forge json-mode markdown-mode js2-refactor rainbow-delimiters objed smooth-scrolling yasnippet-snippets writeroom-mode which-key use-package robot-mode restart-emacs magit leetcode ivy-yasnippet general flycheck exec-path-from-shell evil-surround evil-commentary evil-collection evil-args editorconfig doom-themes doom-modeline diminish diff-hl counsel company avy add-node-modules-path)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
